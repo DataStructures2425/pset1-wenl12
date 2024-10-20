83b0d919d1c6fff9ad524f5df783ec536c54e9f1
Wentong Liu <wentong.liu@wentongliu.local>
2024-10-19 22:45:44 -0400

    i give up

--- testpset1.cpp
+++ testpset1.cpp
@@ -4,6 +4,27 @@ using namespace std;
 
 int main()
 {
+
     init_kalloc();
+
+    void* ptr1 = kalloc(0);
+    void* ptr2 = kalloc(1 << 21);
+    void* ptr3 = kalloc(1 << 14);
+    kfree(ptr2);
+    void* ptr4 = kalloc(1 << 14);
+    void* ptr5 = kalloc(1 << 14);
+
+    kfree(ptr3);
+    kfree(ptr5);
+    kfree(ptr4);
+
+    void* ptr6 = kalloc(1 << 21);
+    kfree(ptr6);
+    
+    void* ptr7 = kalloc(1 << 21);
+    kfree(ptr7);
+
+    void* ptr8 = kalloc(1 << 20);
+    void* ptr9 = kalloc(1 << 19);
 
 }
